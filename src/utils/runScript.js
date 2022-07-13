f4c0f90fe3cfab947c0f3f047042b8f22415fbcc
Sebastian Fripp <irubyxrubyx@gmail.com>
2022-07-13 10:16:04 -0300

    Update runScript.js

--- src/utils/runScript.js
+++ src/utils/runScript.js
@@ -1,24 +1,37 @@
 const { rejects } = require('assert');
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
+const { resolve } = require('path');
 const { stdout } = require("process")
 
+// const runScript = (firstClass, economy, children, hours) => {
+//     return new Promise((resolve, reject) => {
+//         let test
+//         const script = exec(`/usr/bin/bash /var/www/html/pagina/src/jobRun.sh ${firstClass} ${economy} ${children} ${hours}`, (error, stdout, stderr) => {
+//             test = stdout.split("'")
+//             test = test[test.length - 2]
+//             test = test.replace("get -n", "logs --jobrun")
+
+//             setTimeout(() => {
+//                 const script = exec(`test`,
+//                     (error, stdout, stderr) => {
+//                         test = stdout
+//                         test ? resolve(test) : reject("Script execution failed")
+//                     })
+//             }, 15000)
+//         })
+//     })
+// }
+
 const runScript = (firstClass, economy, children, hours) => {
-    return new Promise((resolve, reject) => {
-        let test
-        const script = exec(`/usr/bin/bash /var/www/html/pagina/src/jobRun.sh ${firstClass} ${economy} ${children} ${hours}`, (error, stdout, stderr) => {
-            test = stdout.split("'")
-            test = test[test.length - 2]
-            test = test.replace("get -n", "logs --jobrun")
-            
-            setTimeout(() => {
-                const script = exec(`test`,
-                    (error, stdout, stderr) => {
-                        test = stdout
-                        test ? resolve(test) : reject("Script execution failed")
-                    })
-            }, 15000)
-        })
-    })
+    let test
+    const script = execSync(`/usr/bin/bash /var/www/html/pagina/src/jobRun.sh ${firstClass} ${economy} ${children} ${hours}`)
+    test = script.split("'")
+    test = test[test.length - 2]
+    test = test.replace("get -n", "logs --jobrun")
+
+    script = execSync(`test`)
+    test = script
+    return test
 }
 
 module.exports = runScript
